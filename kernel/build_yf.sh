278732477b509840c210e10b0fa627601e88955d
andr7e <andr7e@mail.com>
2015-01-18 17:12:17 +0300

    +build scripts

--- kernel/build_yf.sh
+++ kernel/build_yf.sh
@@ -8,18 +8,6 @@ export LOCALVERSION=
 #export INSTALL_MOD_PATH=~/kernel_build/rk3188/mod_fw
 
 #board name
-#./mk.sh "mp81"
-
-cp arch/arm/configs/rk3188_yf_defconfig .config
-  
-make menuconfig
-
-make clean
-
-make -j6 Image
-
-#rm -rf $INSTALL_MOD_PATH
-#make -j6 modules
-#make modules_install
+./mk.sh "mp81"
 
 
