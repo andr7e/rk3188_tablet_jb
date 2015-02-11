5f3e24f03bf37529e4ea2543c6cb101b8f574b6b
andr7e <andr7e@mail.com>
2015-02-11 18:52:37 +0300

    update

--- kernel/build_yf.sh
+++ kernel/build_yf.sh
@@ -4,7 +4,7 @@ export PATH=~/kernel_build/arm-eabi-4.6/bin/:$PATH
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel_build/arm-eabi-4.6/bin/arm-eabi-
-export LOCALVERSION=
+export LOCALVERSION=+
 #export INSTALL_MOD_PATH=~/kernel_build/rk3188/mod_fw
 
 #board name
